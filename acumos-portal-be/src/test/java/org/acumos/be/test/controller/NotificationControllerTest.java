d42f9c31dbc044444603a8ac1a0a392575a661e1
unknown <MT00367339@techmahindra.com>
2018-01-09 15:30:55 +0530

    updated code changes for version
    Change-Id: I342d323bfe44b668fa9af2b47d28a6e9e8ee88d6
    Signed-off-by: unknown <MT00367339@techmahindra.com>

--- acumos-portal-be/src/test/java/org/acumos/be/test/controller/NotificationControllerTest.java
+++ acumos-portal-be/src/test/java/org/acumos/be/test/controller/NotificationControllerTest.java
@@ -33,124 +33,128 @@ import org.acumos.cds.transport.RestPageRequest;
 import org.acumos.portal.be.common.JsonRequest;
 import org.acumos.portal.be.common.JsonResponse;
 import org.acumos.portal.be.controller.NotificationController;
+import org.acumos.portal.be.service.NotificationService;
+import org.acumos.portal.be.service.impl.NotificationServiceImpl;
 import org.acumos.portal.be.transport.MLNotification;
 import org.acumos.portal.be.util.EELFLoggerDelegate;
 import org.acumos.portal.be.util.PortalUtils;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class NotificationControllerTest {
 
 	private static final EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(NotificationControllerTest.class);
 
-
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	@InjectMocks
+	NotificationController notificationController;
+	@Mock
+	NotificationServiceImpl notificationService;
+	@Mock
+	NotificationService notificationServiceImpl;
+	
 	final HttpServletResponse response = new MockHttpServletResponse();
 	final HttpServletRequest request = new MockHttpServletRequest();
 
-	@Mock
-	NotificationController notificationController = new NotificationController();
-
 	@Test
-	public void createNotificationTest() {
-		try {
-			MLPNotification mlpNotification = new MLPNotification();
-			Date created = new Date();
-			mlpNotification.setCreated(created);
-			mlpNotification.setMessage("notification created");
-			Date modified = new Date();
-			mlpNotification.setModified(modified);
-			mlpNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
-			mlpNotification.setTitle("Notification");
-			mlpNotification.setUrl("http://notify.com");
-			Assert.assertNotNull(mlpNotification);
-			JsonRequest<MLPNotification> notificationReq = new JsonRequest<>();
-			notificationReq.setBody(mlpNotification);
-			Assert.assertNotNull(notificationReq);
-			JsonResponse<MLNotification> notificationres = new JsonResponse<>();
-			MLNotification responseBody = PortalUtils.convertToMLNotification(mlpNotification);
-			notificationres.setResponseBody(responseBody);
-			Assert.assertNotNull(responseBody);
-			Mockito.when(notificationController.createNotification(request, notificationReq, response))
-					.thenReturn(notificationres);
-			logger.info("Notification Response : " + notificationres.getResponseBody());
-			Assert.assertNotNull(notificationres);
-		} catch (Exception e) {
-			logger.info("failed tot execute the above test case");
-		}
+	public void createNotificationTest(){
 
-	}
-
-	@Test
-	public void getNotificationsTest() {
-		try {
-			MLNotification mlNotification = new MLNotification();
-			mlNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
-			mlNotification.setCount(1);
-			mlNotification.setMessage("notification");
-			mlNotification.setTitle("Notification");
-			mlNotification.setUrl("http://notify.com");
-			Assert.assertNotNull(mlNotification);
-			List<MLNotification> mlNotificationList = new ArrayList<MLNotification>();
-			mlNotificationList.add(mlNotification);
-			Assert.assertNotNull(mlNotificationList);
-			JsonResponse<List<MLNotification>> notificationres = new JsonResponse<>();
-			notificationres.setResponseBody(mlNotificationList);
-			Mockito.when(notificationController.getNotifications()).thenReturn(notificationres);
-			logger.info("Get Notifications : " + notificationres.getResponseBody());
-			Assert.assertNotNull(notificationres);
-		} catch (Exception e) {
-			logger.info("failed tot execute the above test case");
+		MLPNotification mlpNotification = new MLPNotification();
+		JsonResponse<MLNotification> data = new JsonResponse<>();
+		Date created = new Date();
+		mlpNotification.setCreated(created);
+		mlpNotification.setMessage("notification created");
+		Date modified = new Date();
+		mlpNotification.setModified(modified);
+		mlpNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
+		mlpNotification.setTitle("Notification");
+		mlpNotification.setUrl("http://notify.com");
+		JsonRequest<MLPNotification> notificationReq = new JsonRequest<>();
+		notificationReq.setBody(mlpNotification);
+		MLNotification value = PortalUtils.convertToMLNotification(mlpNotification);
+		when(notificationService.createNotification(mlpNotification)).thenReturn(value);
+		data = notificationController.createNotification(request, notificationReq, response);
+		data.setResponseBody(value);
+		if(data != null){
+			logger.debug(EELFLoggerDelegate.debugLogger, "Notification created Successfully :  "+data.getResponseBody());
+		}else {
+			logger.error(EELFLoggerDelegate.errorLogger, "Error Occurred createNotification :");
 		}
 	}
-
+	
 	@Test
-	public void getUserNotificationsTest() {
-		try {
-			MLPUserNotification mlpUserNotification = new MLPUserNotification();
-			Date created = new Date();
-			mlpUserNotification.setCreated(created);
-			mlpUserNotification.setMessage("notification created");
-			Date modified = new Date();
-			mlpUserNotification.setModified(modified);
-			mlpUserNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
-			mlpUserNotification.setTitle("Notification");
-			mlpUserNotification.setUrl("http://notify.com");
-			Date viewed = new Date();
-			mlpUserNotification.setViewed(viewed);
-			Assert.assertNotNull(mlpUserNotification);
-			MLPNotifUserMap mlpNotificationUserMap = new MLPNotifUserMap();
-			mlpNotificationUserMap.setNotificationId(mlpUserNotification.getNotificationId());
-			mlpNotificationUserMap.setUserId("41058105-67f4-4461-a192-f4cb7fdafd34");
-			Assert.assertNotNull(mlpNotificationUserMap);
-			List<MLPUserNotification> mlpUserNotificationList = new ArrayList<>();
-			mlpUserNotificationList.add(mlpUserNotification);
-			Assert.assertNotNull(mlpUserNotificationList);
-			String userId = mlpNotificationUserMap.getUserId();
-			JsonRequest<RestPageRequest> restPageReq = new JsonRequest<>();
-			RestPageRequest body = new RestPageRequest();
-			body.setPage(0);
-			body.setSize(9);
-			JsonResponse<List<MLPUserNotification>> notifires = new JsonResponse<>();
-			if (body.getPage() != null || body.getSize() != null) {
-				notifires.setResponseBody(mlpUserNotificationList);
-			}
-
-			Mockito.when(notificationController.getUserNotifications(request, userId, restPageReq, response))
-					.thenReturn(notifires);
-			logger.info("User notifications  : " + notifires.getResponseBody());
-			Assert.assertNotNull(notifires);
-		} catch (Exception e) {
-			logger.info("failed tot execute the above test case");
+	public void getNotifications(){
+		MLNotification mlNotification = new MLNotification();
+		mlNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
+		mlNotification.setCount(1);
+		mlNotification.setMessage("notification");
+		mlNotification.setTitle("Notification");
+		mlNotification.setUrl("http://notify.com");
+		Assert.assertNotNull(mlNotification);
+		List<MLNotification> mlNotificationList = new ArrayList<MLNotification>();
+		mlNotificationList.add(mlNotification);
+		Assert.assertNotNull(mlNotificationList);
+		JsonResponse<List<MLNotification>> notificationres = new JsonResponse<>();
+		notificationres.setResponseBody(mlNotificationList);
+		when(notificationService.getNotifications()).thenReturn(mlNotificationList);
+		notificationres = notificationController.getNotifications();
+		if(notificationres != null){
+			logger.debug(EELFLoggerDelegate.debugLogger, "getNotifications: size is {} ", mlNotificationList.size());
+		}else {
+			logger.error(EELFLoggerDelegate.errorLogger, "Error Occurred while fetching Notification :");
 		}
 	}
-
+	
+	@Test
+	public void getUserNotifications(){
+		MLPUserNotification mlpUserNotification = new MLPUserNotification();
+		Date created = new Date();
+		mlpUserNotification.setCreated(created);
+		mlpUserNotification.setMessage("notification created");
+		Date modified = new Date();
+		mlpUserNotification.setModified(modified);
+		mlpUserNotification.setNotificationId("037ad773-3ae2-472b-89d3-9e185a2cbfc9");
+		mlpUserNotification.setTitle("Notification");
+		mlpUserNotification.setUrl("http://notify.com");
+		Date viewed = new Date();
+		mlpUserNotification.setViewed(viewed);
+		Assert.assertNotNull(mlpUserNotification);
+		MLPNotifUserMap mlpNotificationUserMap = new MLPNotifUserMap();
+		mlpNotificationUserMap.setNotificationId(mlpUserNotification.getNotificationId());
+		mlpNotificationUserMap.setUserId("41058105-67f4-4461-a192-f4cb7fdafd34");
+		Assert.assertNotNull(mlpNotificationUserMap);
+		List<MLPUserNotification> mlpUserNotificationList = new ArrayList<>();
+		mlpUserNotificationList.add(mlpUserNotification);
+		Assert.assertNotNull(mlpUserNotificationList);
+		String userId = mlpNotificationUserMap.getUserId();
+		JsonRequest<RestPageRequest> restPageReq = new JsonRequest<>();
+		RestPageRequest restPageRequest = new RestPageRequest();
+		restPageRequest.setPage(0);
+		restPageRequest.setSize(9);
+		JsonResponse<List<MLPUserNotification>> notifires = new JsonResponse<>();
+		if (restPageRequest.getPage() != null || restPageRequest.getSize() != null) {
+			notifires.setResponseBody(mlpUserNotificationList);
+		}
+		
+		when(notificationService.getUserNotifications(userId, restPageRequest)).thenReturn(mlpUserNotificationList);
+		notifires = notificationController.getUserNotifications(request, userId, restPageReq, response);
+		if(notifires != null){
+			logger.debug(EELFLoggerDelegate.debugLogger, "No notifications exist for user : "+userId);
+		}else {
+			logger.error(EELFLoggerDelegate.errorLogger, "Exception Occurred while getUserNotifications");
+		}
+	}
+	
 	@Test
 	public void addNotificationUserTest() {
 		try {
@@ -176,15 +180,15 @@ public class NotificationControllerTest {
 			String notificationId = mlpNotificationUserMap.getNotificationId();
 			Assert.assertNotNull(userId);
 			Assert.assertNotNull(notificationId);
-			Mockito.when(notificationController.addNotificationUser(request, notificationId, userId, response))
-					.thenReturn(notificationres);
+			NotificationServiceImpl mockImpl = mock(NotificationServiceImpl.class);
+			mockImpl.addNotificationUser(notificationId, userId);
+			notificationController.addNotificationUser(request, notificationId, userId, response);
 			logger.info("Successfully  added notifiaction for particular user : " + notificationres.getResponseBody());
 			Assert.assertNotNull(notificationres);
 		} catch (Exception e) {
 			logger.info("failed tot execute the above test case");
 		}
 	}
-
 	@Test
 	public void dropNotificationUserTest() {
 		try {
@@ -210,8 +214,9 @@ public class NotificationControllerTest {
 			String notificationId = mlpNotificationUserMap.getNotificationId();
 			Assert.assertNotNull(userId);
 			Assert.assertNotNull(notificationId);
-			Mockito.when(notificationController.dropNotificationUser(request, notificationId, userId, response))
-					.thenReturn(notificationres);
+			NotificationServiceImpl mockImpl = mock(NotificationServiceImpl.class);
+			mockImpl.dropNotificationUser(notificationId, userId);
+			notificationController.dropNotificationUser(request, notificationId, userId, response);
 			logger.info("Successfully  droped notifiaction for particular user : " + notificationres.getResponseBody());
 			Assert.assertNotNull(notificationres);
 		} catch (Exception e) {
@@ -245,8 +250,9 @@ public class NotificationControllerTest {
 			String notificationId = mlpNotificationUserMap.getNotificationId();
 			Assert.assertNotNull(userId);
 			Assert.assertNotNull(notificationId);
-			Mockito.when(notificationController.setNotificationUserViewed(request, notificationId, userId, response))
-					.thenReturn(notificationres);
+			NotificationServiceImpl mockImpl = mock(NotificationServiceImpl.class);
+			mockImpl.setNotificationUserViewed(notificationId, userId);
+			notificationController.setNotificationUserViewed(request, notificationId, userId, response);
 			logger.info("Successfully  setNotificationUserViewed: " + notificationres.getResponseBody());
 			Assert.assertNotNull(notificationres);
 		} catch (Exception e) {
@@ -281,8 +287,9 @@ public class NotificationControllerTest {
 			Assert.assertNotNull(userId);
 			String notificationId = mlpNotificationUserMap.getNotificationId();
 			Assert.assertNotNull(notificationId);
-			Mockito.when(notificationController.deleteNotification(request, notificationId, response))
-					.thenReturn(notificationres);
+			NotificationServiceImpl mockImpl = mock(NotificationServiceImpl.class);
+			mockImpl.dropNotificationUser(notificationId, userId);
+			notificationController.deleteNotification(request, notificationId, response);
 			logger.info("Successfully  setNotificationUserViewed: " + notificationres.getResponseBody());
 			Assert.assertNotNull(notificationres);
 		} catch (Exception e) {
@@ -304,7 +311,8 @@ public class NotificationControllerTest {
 			Assert.assertNotNull(mlNotification);
 			JsonResponse<MLNotification> notificationres = new JsonResponse<>();
 			notificationres.setResponseBody(mlNotification);
-			Mockito.when(notificationController.getNotificationCount()).thenReturn(notificationres);
+			when(notificationServiceImpl.getNotificationCount());
+			notificationres = notificationController.getNotificationCount();
 			logger.info("Successfully  setNotificationUserViewed: " + notificationres.getResponseBody());
 			Assert.assertNotNull(notificationres);
 		} catch (Exception e) {
