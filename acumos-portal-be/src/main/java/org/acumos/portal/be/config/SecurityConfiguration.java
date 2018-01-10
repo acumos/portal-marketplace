2ecd30ce94398868b0803ba2f88fc5319824bc43
Vinayak Shetage <vs00509519@techmahindra.com>
2018-01-10 16:15:52 +0530

    BE changes to add shared model in My model page
    Change-Id: Ib21fe888f1dda5111735787a5488fe0f13c1b360
    Signed-off-by: Vinayak Shetage <vs00509519@techmahindra.com>

--- acumos-portal-be/src/main/java/org/acumos/portal/be/config/SecurityConfiguration.java
+++ acumos-portal-be/src/main/java/org/acumos/portal/be/config/SecurityConfiguration.java
@@ -108,7 +108,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .antMatchers("/users/qAUrl").permitAll()
         .antMatchers("/users/docs").permitAll()
         .antMatchers("/users/userProfileImage/{userId}").permitAll()  
-        .antMatchers("/admin/version").permitAll() 
+        .antMatchers("/admin/version").permitAll()   
         .anyRequest().authenticated();
     	
     	// Custom JWT based authentication
