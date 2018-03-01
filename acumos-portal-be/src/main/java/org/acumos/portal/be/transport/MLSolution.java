0814e8510670f4d54e3ad461226988258df8d7a4
Jincy Bharill <jb00494472@techmahindra.com>
2018-03-01 20:59:31 +0530

    Federation Changes
    Issue-ID: CD-2080
    
    Description: Changes for passing the selector without any slashes for adding subscription.
    
    Change-Id: If643190ba0c73f2e07ce762b092da377927c69fd
    Signed-off-by: Jincy Bharill <jb00494472@techmahindra.com>

--- acumos-portal-be/src/main/java/org/acumos/portal/be/transport/MLSolution.java
+++ acumos-portal-be/src/main/java/org/acumos/portal/be/transport/MLSolution.java
@@ -72,6 +72,7 @@ public class MLSolution {
 	private List<MLPThread> threadList ;
 	private String validationStatusCode;	
     private Long refreshInterval;
+    private String selector;
 	
 	public String getLoginName() {
 		return loginName;
@@ -483,5 +484,11 @@ public class MLSolution {
 		this.refreshInterval = refreshInterval;
 	}
 
-	
+	public String getSelector() {
+		return selector;
+	}
+
+	public void setSelector(String selector) {
+		this.selector = selector;
+	}
 }
