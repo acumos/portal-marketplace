f80a5b6d2335490a6ff3467d76c5a2b9d69c396b
Neeraj Sonar <ns00499861@techmahindra.com>
2018-04-11 20:18:37 +0530

    Validation for Model Name.
    Issue-ID: ACUMOS-395, ACUMOS-411, ACUMOS-576.
    Description: Fixed- Validation for Model Name, Account Setting : Upload Image Button, Deactivate-account feature should request confirmation.
    
    Change-Id: Ib3e9dffa1c9fd0bb1b336d7d07101253590b2224
    Signed-off-by: Neeraj Sonar <ns00499861@techmahindra.com>

--- acumos-portal-fe/src/main/webapp/app/userDetail/userDetail.component.js
+++ acumos-portal-fe/src/main/webapp/app/userDetail/userDetail.component.js
@@ -84,14 +84,6 @@ angular
 						}
 						getUserDetail();
 						
-						/*check if user is of LF*/
-						$scope.isLfUser = false;
-						$scope.sessionLFCAS = sessionStorage.getItem('provider');
-						if($scope.sessionLFCAS == "LFCAS"){
-							$scope.isLfUser = true;
-						}else{
-							$scope.isLfUser = false;
-						}
 
 						// Get User data
 						
@@ -114,8 +106,8 @@ angular
 											function(data, status, headers,
 													config) {
 												if (data.status) {
-													$scope.userImage = data.response_body;
-													$scope.userPic = $scope.userImage;
+													//$scope.userImage = data.response_body;
+													$scope.userPic = data.response_body; //$scope.userImage;
 													$scope.showAltImage = false;
 												}
 											}).error(
@@ -328,7 +320,21 @@ angular
 						}
 						// Update User Detail
 						$scope.imageurl = "images/profile-icon-01-48X48.png";
+						
 						// Update/Deactivate account
+						
+						/*Deactivate Account Confirmation Pop-up*/
+						$scope.dialogDeactivateAccount = function(ev) {
+		                	  $scope.error = false;
+			                	$mdDialog.show({
+			                      contentElement: '#dialogDeactivateAccount',
+			                      parent: angular.element(document.body),
+			                      targetEvent: ev,
+			                      clickOutsideToClose: true
+			                    });
+						};
+						
+						/*Deactivate Account*/
 						$scope.updateDeactivate = function(value) {
 							
 							if (value === "deactivate") {
