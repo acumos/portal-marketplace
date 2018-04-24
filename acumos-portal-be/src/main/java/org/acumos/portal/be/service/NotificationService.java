fd02c70f59ba5df9f25d1c0813d4e05c2b831807
RAIKWAR <VR00122945@techmahindra.com>
2018-04-23 19:06:07 -0500

    Added notification service
    Issue ID: ACUMOS-145
    
    Added notification service that will notifiy user based on the
    preferences.
    
    Change-Id: I97c42a9c8bcf7fd82ed8c5f4c2dfbeaa5adc614d
    Signed-off-by: RAIKWAR <VR00122945@techmahindra.com>

--- acumos-portal-be/src/main/java/org/acumos/portal/be/service/NotificationService.java
+++ acumos-portal-be/src/main/java/org/acumos/portal/be/service/NotificationService.java
@@ -25,33 +25,111 @@ import java.util.List;
 import org.acumos.cds.domain.MLPNotification;
 import org.acumos.cds.domain.MLPUserNotification;
 import org.acumos.cds.transport.RestPageRequest;
+import org.acumos.portal.be.common.exception.AcumosServiceException;
 import org.acumos.portal.be.transport.MLNotification;
-import org.acumos.portal.be.transport.MLUserNotifPref; 
+import org.acumos.portal.be.transport.MLUserNotifPref;
+import org.acumos.portal.be.transport.NotificationRequestObject; 
+
 
 public interface NotificationService {
-
+	/**
+	 * Creates the notification in DB
+	 * 
+	 * @param body
+	 *            MLPNotification
+	 * @return MLNotification
+	 */
 	MLNotification createNotification(MLPNotification body);
 
+	/**
+	 * Fetch all the notifications from the DB
+	 * 
+	 * @return List of paginated MLNotification
+	 */
 	List<MLNotification> getNotifications();
 
+	/**
+	 * Get the List of notifications for a user
+	 * 
+	 * @param userId
+	 * @param restPageRequest
+	 * @return List of notifications for user
+	 */
 	List<MLPUserNotification> getUserNotifications(String userId, RestPageRequest restPageRequest);
 
+	/**
+	 * Add the generated notification to the user
+	 * 
+	 * @param notificationId
+	 * @param userId
+	 */
 	void addNotificationUser(String notificationId, String userId);
 
+	/**
+	 * Delete the notification for the user
+	 * 
+	 * @param notificationId
+	 * @param userId
+	 */
 	void dropNotificationUser(String notificationId, String userId);
 
+	/**
+	 * Mark the notification as viewed for the user
+	 * 
+	 * @param notificationId
+	 * @param userId
+	 */
 	void setNotificationUserViewed(String notificationId, String userId);
-	
+
+	/**
+	 * @param notificationId
+	 */
 	void deleteNotification(String notificationId);
 
+	/**
+	 * Get Count of all the notifications
+	 * 
+	 * @return Number of notifications
+	 */
 	int getNotificationCount();
 
+	/**
+	 * Generate a notification for the user
+	 * 
+	 * @param notification
+	 * @param userId
+	 */
 	void generateNotification(MLPNotification notification, String userId);
-	
-    List<MLUserNotifPref> getUserNotifPrefByUserId(String userId);
-    
-    MLUserNotifPref createUserNotificationPreference(MLUserNotifPref mlpUserNotifPref);
-       
-    void updateUserNotificationPreference(MLUserNotifPref mlpUserNotifPref);
+
+	/**
+	 * Get the notification preferences for the user
+	 * 
+	 * @param userId
+	 * @return List of notification preferences for the user
+	 */
+	List<MLUserNotifPref> getUserNotifPrefByUserId(String userId);
+
+	/**
+	 * Create a notification preference for the user
+	 * 
+	 * @param mlpUserNotifPref
+	 * @return Notification preference 
+	 */
+	MLUserNotifPref createUserNotificationPreference(MLUserNotifPref mlpUserNotifPref);
+
+	/**
+	 * Update the notification preference
+	 * 
+	 * @param mlpUserNotifPref
+	 */
+	void updateUserNotificationPreference(MLUserNotifPref mlpUserNotifPref);
+
+	/**
+	 * Send the notification to the user according to the notification preference
+	 * 
+	 * @param notificationRequest
+	 * @throws AcumosServiceException
+	 */
+	void sendUserNotification(NotificationRequestObject notificationRequest) throws AcumosServiceException;
 
 }
