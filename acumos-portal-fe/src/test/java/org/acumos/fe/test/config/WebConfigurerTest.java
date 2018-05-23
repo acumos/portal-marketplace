e371e2d54f3f82a33d1c9b286daa3ae5765610eb
unknown <SS00492019@TechMahindra.com>
2018-05-23 13:01:28 +0530

    Junit Added in Portal-be
    Change-Id: Idb7a1f140951276650218e98ebab82ae2ee8e59f
    Issue-ID: ACUMOS-571
    Signed-off-by: unknown <SS00492019@TechMahindra.com>

---
+++ acumos-portal-fe/src/test/java/org/acumos/fe/test/config/WebConfigurerTest.java
@@ -0,0 +1,80 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+package org.acumos.fe.test.config;
+
+import java.io.File;
+import java.util.List;
+
+import org.acumos.portal.fe.AcumosPortalApplication;
+import org.acumos.portal.fe.config.EELFLoggerDelegate;
+import org.acumos.portal.fe.config.WebConfigurer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.context.embedded.AbstractConfigurableEmbeddedServletContainer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+
+public class WebConfigurerTest {
+	
+	 private static final int _DEFAULT_PORT = 8080;
+
+	private final static EELFLoggerDelegate  logger = EELFLoggerDelegate.getLogger(AcumosPortalApplication.class);
+	
+	 private AbstractConfigurableEmbeddedServletContainer dummyconfigurableEmbeddedServletContainer = new AbstractConfigurableEmbeddedServletContainer() {
+		 
+		private File root;
+		
+		@Override
+		public void setPort(int port) {
+					
+		}		
+
+		@Override
+		public void setDocumentRoot(File documentRoot) {
+			root = documentRoot;			
+		}
+
+		@Override
+		public void setInitializers(List<? extends ServletContextInitializer> initializers) {
+						
+		}
+
+		@Override
+		public void addInitializers(ServletContextInitializer... initializers) {
+						
+		}		
+
+		@Override
+		public File getDocumentRoot() {
+			return root;	
+		}		
+	};		
+	
+	@Test
+    public void contextLoads() throws Exception {	
+		
+		logger.info("Started contextLoads()");		
+		WebConfigurer webConfigurer = new WebConfigurer();
+		webConfigurer.customize(dummyconfigurableEmbeddedServletContainer);
+		Assert.assertNotNull(dummyconfigurableEmbeddedServletContainer.getDocumentRoot());
+        Assert.assertEquals(dummyconfigurableEmbeddedServletContainer.getPort(), _DEFAULT_PORT);
+		logger.info("Finished  contextLoads()");
+    }
+}
---
+++ acumos-portal-fe/src/test/java/org/acumos/fe/test/controller/MainControllerTest.java
@@ -0,0 +1,43 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+package org.acumos.fe.test.controller;
+
+import org.acumos.portal.fe.AcumosPortalApplication;
+import org.acumos.portal.fe.controller.MainController;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AcumosPortalApplication.class)
+public class MainControllerTest {
+	
+	private MainController  mainController  = new MainController ();
+
+	@Test
+	public void testHomepage() throws Exception {
+		
+		String status = mainController.homepage();
+		Assert.assertEquals(status, "index.html");
+	}	 
+	
+}
