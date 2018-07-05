fc6746bee2accaf5fab6e597cab795c84f86aa72
Nilesh Patil4 <NP00490312@techmahindra.com>
2018-07-05 16:25:29 +0530

    average rating display
    Change-Id: I5e27f499f5c396b989dbb316459b825f73c60005
    ISSUE-ID: ACUMOS-1068
    Signed-off-by: Nilesh Patil4 <NP00490312@techmahindra.com>

--- acumos-portal-fe/src/main/webapp/app/market_place/manage-module.component.js
+++ acumos-portal-fe/src/main/webapp/app/market_place/manage-module.component.js
@@ -17,7 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ===============LICENSE_END=========================================================
 */
-
+ 
 'use strict';
  
 angular.module('manageModule')
