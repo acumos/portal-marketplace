553ae98f2692607cf4d31a05a7721830973ef91f
Nilesh Patil4 <NP00490312@techmahindra.com>
2018-07-05 13:47:42 +0530

    Adding test case.
    Change-Id: I2c146f248bfd48131f774bcf16fc76dfc114af9e
    Issue-ID: Acumos-1068
    Signed-off-by: Nilesh Patil4 <NP00490312@techmahindra.com>

--- acumos-portal-be/src/test/java/org/acumos/be/test/transport/MLSolutionTest.java
+++ acumos-portal-be/src/test/java/org/acumos/be/test/transport/MLSolutionTest.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ===============LICENSE_END=========================================================
- */
+ */ 
 package org.acumos.be.test.transport;
 
 import java.util.ArrayList;
@@ -68,7 +68,7 @@ public class MLSolutionTest {
 		 String threadId= "th1001";
 		 String commentId="cm2001";
 		 List<MLPThread> threadList = new ArrayList<MLPThread>();
-		 int solutionRatingAvg=3;
+		 float solutionRatingAvg=3;
 				
 		MLPSolutionRevision mlPSolutionRevision = new MLPSolutionRevision();
 		mlPSolutionRevision.setCreated(new Date());
