a06b0ffc503c1af989b5c7d2fe3936692da879da
swood8 <SW0C74397@TechMahindra.com>
2018-08-09 13:17:36 -0500

    Fix recognition of mobile device versus desktop
    Issue-ID: ACUMOS-1549
    Added a fix which allows Acumos to identify whether or not the user is
    accessing the site using a mobile device or a desktop browser; if the
    latter is used, the "Acumos is not mobile friendly" message will not be
    displayed.
    
    Change-Id: Ib097245e87517e75aea102c07bfa2337f0eea50e
    Signed-off-by: swood8 <SW0C74397@TechMahindra.com>

--- acumos-portal-fe/src/main/webapp/app/app.config.js
+++ acumos-portal-fe/src/main/webapp/app/app.config.js
@@ -20,11 +20,7 @@ limitations under the License.
 
 'use strict';
 
-/*
- * angular .module('AcumosApp', ['ui.router'])
- */
-app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, ngQuillConfigProvider, $authProvider/*, socialProvider*/){
-	//$locationProvider.html5Mode(true);
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, ngQuillConfigProvider, $authProvider){
 	ngQuillConfigProvider.set();
 	$urlRouterProvider.when('','/home');
 	$urlRouterProvider.otherwise('/404Error');
@@ -32,9 +28,7 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $location
 	$stateProvider
 		 .state('404Error', {
 	         url: '/404Error',
-	         //template:'<h1>404 Error</h1>',
 	         templateUrl: '/app/error-page/error-404.template.html',
-	         //component: '404Error'
 	     })
 		.state('home', {
 			url: '/home',
@@ -66,7 +60,6 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $location
 			url: '/designStudio',
 			component: 'designStudio'
 		})
-		// marketSolutions
 		.state('marketSolutions', {
 			url: '/marketSolutions?solutionId&revisionId',
 			component: 'modelDetails',
@@ -75,8 +68,6 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $location
 				revisionId : null,
 				parentUrl: null
 				}
-			//template: '<model-details></model-details>'
-				//templateUrl: './app/model-details/model-details.template.html' 
          })
           .state('modelEdit', {
 			url: '/modelEdit',
@@ -130,37 +121,26 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $location
 	          scopePrefix: 'openid',
 	          scopeDelimiter: ' ',
 	          popupOptions: { width: 500, height: 600 },
-	          //optionalUrlParams: ['access_type'],
 	          accessType: 'offline'
 
 	    });
 
 	    $authProvider.google({
-	    	 // clientId: '268793520077-crpk6sspb8rob0gs33cgh3hjdfah4iv6.apps.googleusercontent.com',
-	         // clientId: '719388764800-sm4ddoku49cl88dl47h0s031968lvd6l.apps.googleusercontent.com',
-	          //vlcH1U3F0KGuycEesB0et37K
-	          //clientId:'327439217798-e05g1c5ive01i867ikc1lrvcc4tjcsk0.apps.googleusercontent.com',
 	    	  clientId:'805276053124-mohnn1eltto7phq51mko2runo7aa04n6.apps.googleusercontent.com',
 	          authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
 	    	  url: 'http://localhost:8083/oauth/login',
 	          responseType: 'token',
-	          //default:'code',
-	          //redirectUri: window.location.origin,//'http://localhost:8085', //http://localhost:8085/callback //url given in google api configuration. this should match 
 	          requiredUrlParams: ['scope'],
 	          optionalUrlParams: ['display'],
 	          scope: ['profile', 'email'],
 	          scopePrefix: 'openid',
 	          scopeDelimiter: ' ',
 	          popupOptions: { width: 500, height: 600 },
-	          //optionalUrlParams: ['access_type'],
 	          accessType: 'offline'
 	        });
 	    $authProvider.github({
-	          /*clientId: '5a1c39acf1e13a80b5e1',
-	          redirectUri: '/manageModule' //http://localhost:8085/callback
-	           */
 	    	clientId:'1587275085f20e9a68bc',
-    		redirectUri: window.location.origin, //http://localhost:8085/callback
+    		redirectUri: window.location.origin,
           authorizationEndpoint:'https://github.com/login/oauth/authorize',
           scope: ['user'],
           url:'http://localhost:8085',
@@ -170,39 +150,26 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $location
           responseType: 'token'
 	    	});
 	    
-	    /*socialProvider.setGoogleKey("719388764800-sm4ddoku49cl88dl47h0s031968lvd6l.apps.googleusercontent.com");*/
-	    
 }).run(function($rootScope, $state, $location, browserStorageService) {
     $rootScope.$state = $state;
     $rootScope.$on("$locationChangeStart", function(event, next, current) { 
-        /*if($location.path() === "/home"){
-        	$rootScope.toggleSideNavBar = false;
-        } else {
-        	$rootScope.toggleSideNavBar = true;
-        }*/	
-    		/*This is added for resolving mdl search related issues */
     		componentHandler.upgradeAllRegistered();
     		
-    		//console.log('app.config.js->$rootScope.sidebarHeader: ' + $rootScope.sidebarHeader)
-    	 	$rootScope.sidebarHeader = false;
-
-    	 	//console.log('#######app.config.js->$rootScope.sidebarHeader: ' + $rootScope.sidebarHeader)
-    	 	//$scope.sidebarHeader = false;
+    		$rootScope.sidebarHeader = false;
+    	 	
+    	 	$rootScope.isMobile = false;
+			(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) $rootScope.isMobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
+ 
 		   if (browserStorageService.getUserDetail()) {
-				//console.log("Yes");
 				$rootScope.sidebarHeader = true;
-				//$scope.sidebarHeader = true;
 			}
 		   else{
-			   //console.log("No");
 			   $rootScope.sidebarHeader = false;
-			   //$scope.sidebarHeader = false;
 		   }
 		    angular.element('.mdl-textfield').removeClass('is-focused');
 			angular.element('.mdl-textfield').removeClass('is-upgraded');
 			angular.element('.mdl-textfield').removeClass('is-dirty');
 			angular.element('.mdl-textfield__expandable-holder input').val('');
-			//$rootScope.valueToSearch = '';
         
     });
 })
