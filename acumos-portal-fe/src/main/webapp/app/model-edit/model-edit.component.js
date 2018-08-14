8c74cccdd898e64cb82c337c341d65b0496f545d
swood8 <SW0C74397@TechMahindra.com>
2018-08-14 13:35:06 -0500

    Display user icons in Share My Model search field
    Issue-ID: ACUMOS-1538
    User profile pictures, if set, will now display next to usernames when
    searching for users to add on the Share My Model/With Team page.
    
    Change-Id: Iac3786993dfdb2d747a556ed7d91e6a49322a5cd
    Signed-off-by: swood8 <SW0C74397@TechMahindra.com>

--- acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
+++ acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
@@ -1493,7 +1493,8 @@ angular
 														firstName : item.firstName,
 														lastName : item.lastName,
 														userEmailId : item.emailId,
-														userID : item.userId
+														userID : item.userId,
+														picture : item.picture
 													});
 													
 												});
@@ -1514,7 +1515,8 @@ angular
 										'firstName' : item.firstName,
 										'lastName' : item.lastName,
 										'userId' : item.userID,
-										'emailId' : item.userEmailId
+										'emailId' : item.userEmailId,
+										'picture' : item.picture
 									}); // item.lastName
 								}
 							});
