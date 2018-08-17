7135bed29421cc630b1c78a25c05b1ca2a5e6d4e
Sandeep Suluguri <SS00540557@techmahindra.com>
2018-08-17 10:45:10 -0400

    Repeated Login Failures UI
    Issue-Id: ACUMOS-1491
    Change-Id: I9a81f9e21340a5154cd992c0570413b734c29a84
    Signed-off-by: Sandeep Suluguri <SS00540557@techmahindra.com>

--- acumos-portal-fe/src/main/webapp/app/header/sign-in.component.js
+++ acumos-portal-fe/src/main/webapp/app/header/sign-in.component.js
@@ -151,7 +151,6 @@ angular.module('signInModal')
                                     	  //var $ctrl = this;
                                     	  console.info("in signin controller");
                                     	  $scope.signin = function() {
-
                                                 $scope.userData = {"request_body":{"username": $scope.modalData.name, "password": $scope.modalData.value}}
                                                 sessionStorage.setItem('rm', $scope.modalData.cb1 ? "remember" : "");
                                                 $scope.login();
@@ -258,11 +257,10 @@ angular.module('signInModal')
                                                 }
                                                 $mdDialog.hide();
                                                },function errorCallback(response) {
-                                                   console.log("Error: ", response);
-                                                   $scope.userPassInvalid = true;
-                                                   $scope.userVerificationPending = false;
+                                                   
                                                    if(response.data.message == "Inactive user"){
                                                 	   $scope.userIdDisabled = true;
+                                                	   $scope.userPassBlocked = false;
                                                 	   $scope.userPassInvalid = false;
                                                 	   $scope.userVerificationPending = false;
                                                 	   	 /*$mdDialog.hide();
@@ -271,10 +269,21 @@ angular.module('signInModal')
                                                    if(response.data.message == "Verification Pending"){
                                                 	   $scope.userIdDisabled = false;
                                                 	   $scope.userPassInvalid = false;
+                                                	   $scope.userPassBlocked = false;
                                                 	   $scope.userVerificationPending = true;
                                                 	   	 /*$mdDialog.hide();
                                                          alert("User Id is disabled");*/
                                                    }
+                                                   if(response.data.message.indexOf('blocked') > -1){
+                                                	   $scope.userPassBlocked = true; 
+                                                	   $scope.userBlockedMessage = response.data.message;
+                                                	   $scope.userPassInvalid = false;
+                                                       $scope.userVerificationPending = false;
+                                               	   } else if(response.data.message == "Failed to authenticate user"){
+	                                                   $scope.userPassInvalid = true;
+	                                                   $scope.userPassBlocked = false;
+	                                                   $scope.userVerificationPending = false;
+                                               	   }
                                              });
                                           };  
                                           
