c24e75e1b9b92f00b2eeb5c840aee3367287547f
Nilesh Patil4 <NP00490312@techmahindra.com>
2018-10-03 17:07:35 +0530

    Issue fixing federation.
    Change-Id: Ied8a077100684583cbbee9166636986e5516680f
    Issue-ID: ACUMOS-1552,ACUMOS-1721
    Signed-off-by: Nilesh Patil4 <NP00490312@techmahindra.com>

--- acumos-portal-fe/src/main/webapp/app/app-filters/bytesToSize.filter.js
+++ acumos-portal-fe/src/main/webapp/app/app-filters/bytesToSize.filter.js
@@ -1,10 +1,13 @@
 app.filter('formatBytes', function() {
-	return function(bytes,decimals) {
-		if(bytes == 0) return '0 Bytes';
+	return function(bytes,decimals) {		
+		
+          if(bytes == 0) return '0 Bytes';
+	  if(bytes == -1) return 'N/A';
 		   var k = 1024,
 		       dm = decimals || 2,
 		       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
 		       i = Math.floor(Math.log(bytes) / Math.log(k));
 		   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-	}
+	
+        }
 });
