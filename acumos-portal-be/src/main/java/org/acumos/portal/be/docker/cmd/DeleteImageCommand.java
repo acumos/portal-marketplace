f0064ee59f853957d9010d7fce5d4b25fe692d03
swood8 <SW0C74397@TechMahindra.com>
2019-02-25 13:55:56 -0600

    Fix deletion of artifact docker images
    Applied autowire of docker configuration to ensure that image deletion
    via DeleteImageCommand occurs correctly.
    
    Issue-ID: ACUMOS-2465
    Change-Id: Ibf46d28798624ddee3587eae8da0ef42bec6c837
    Signed-off-by: swood8 <SW0C74397@TechMahindra.com>

--- acumos-portal-be/src/main/java/org/acumos/portal/be/docker/cmd/DeleteImageCommand.java
+++ acumos-portal-be/src/main/java/org/acumos/portal/be/docker/cmd/DeleteImageCommand.java
@@ -8,47 +8,47 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.RemoveImageCmd;
 import com.github.dockerjava.api.exception.DockerException;
 
-public class DeleteImageCommand extends DockerCommand{
+public class DeleteImageCommand extends DockerCommand {
 
 	private final String pathUri;
-  
-        @Autowired
+
+	@Autowired
 	private DockerConfiguration dockerConfiguration;
-			
+
 	public DeleteImageCommand(final String pathUri) {
 		this.pathUri = pathUri;
 	}
-	
+
 	public String getPathUri() {
 		return pathUri;
 	}
-		
+
 	@Override
 	public void execute() throws DockerException {
-		if (pathUri == null || pathUri.isEmpty())
-		{
+		if (pathUri == null || pathUri.isEmpty()) {
 			throw new IllegalArgumentException("pathUri is not configured");
 		}
-		logger.debug("execute docker path :", pathUri);		
-	    String imageFullName = pathUri;
-	    //String imageFullName = CommandUtils.imageFullNameFrom(registry, image, tag); 
-	    
-	    try{
-	    	
+		logger.debug("execute docker path :", pathUri);
+		String imageFullName = pathUri;
+		// String imageFullName = CommandUtils.imageFullNameFrom(registry,
+		// image, tag);
+
+		try {
+
 			DockerClient dockerClient = DockerClientFactory.getDockerClient(dockerConfiguration);
-			System.out.println( "dockerClient=" + dockerClient);
+			System.out.println("dockerClient=" + dockerClient);
 			RemoveImageCmd removeImageCmd = dockerClient.removeImageCmd(imageFullName).withForce(true);
 			removeImageCmd.exec();
-		    logger.info("execute DeleteImageCommand:", removeImageCmd);
-		    		    
-	    }catch (Exception e)
-		{
-			final String msg = String.format("execute failed on DeleteImageCommand:pathUri '%s'", pathUri + ":" + pathUri);
+			logger.info("execute DeleteImageCommand:", removeImageCmd);
+
+		} catch (Exception e) {
+			final String msg = String.format("execute failed on DeleteImageCommand:pathUri '%s'",
+					pathUri + ":" + pathUri);
 			logger.error(msg, e);
 			throw new DockerException(msg, org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR, e);
-		}	    
+		}
 	}
-	
+
 	@Override
 	public String getDisplayName() {
 		// TODO Auto-generated method stub
