5cbf3ab4c19ae3608a81ed7c010827e09bc9847f
Lott, Christopher (cl778h) <cl778h@att.com>
2019-03-12 09:23:53 -0400

    Extend logging in send-mail services
    Change-Id: Id23121ea6468a438623fbdca51f06b5d00e66521
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- acumos-portal-be/src/main/java/org/acumos/portal/be/service/impl/MailJetService.java
+++ acumos-portal-be/src/main/java/org/acumos/portal/be/service/impl/MailJetService.java
@@ -22,19 +22,10 @@
  * This call sends an email to one recipient, using a validated sender address
  */
 package org.acumos.portal.be.service.impl;
-import com.mailjet.client.errors.MailjetException;
-import com.mailjet.client.errors.MailjetSocketTimeoutException;
-import com.mailjet.client.MailjetClient;
-import com.mailjet.client.MailjetRequest;
-import com.mailjet.client.MailjetResponse;
-import com.mailjet.client.resource.Email;
-
-import freemarker.template.Configuration;
 
 import java.util.Map;
 
 import org.acumos.portal.be.service.MailJet;
-import org.acumos.portal.be.service.MailService;
 import org.acumos.portal.be.transport.MailData;
 import org.acumos.portal.be.util.EELFLoggerDelegate;
 import org.acumos.portal.be.util.PortalUtils;
@@ -46,72 +37,79 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mailjet.client.resource.Email;
+
+import freemarker.template.Configuration;
 
 @Service
 public class MailJetService implements MailJet {
-	
-	
-    @Autowired
-    Configuration freemarkerConfiguration;
+
+	private static final EELFLoggerDelegate log = EELFLoggerDelegate.getLogger(MailJetService.class);
+
+	@Autowired
+	private Configuration freemarkerConfiguration;
 
 	@Autowired
 	private Environment env;
-	
-    private static final EELFLoggerDelegate log = EELFLoggerDelegate.getLogger(MailJetService.class);
-	
-    @Override
+
+	@Override
 	public void sendMail(final MailData mailData) {
-      MailjetClient client;
-      MailjetRequest request = null;
-      MailjetResponse response = null;
-      
-      String apiKey = env.getProperty("portal.mailjet.api.key");
-      String secretKey = env.getProperty("portal.mailjet.secret.key");
-      String fromAddress = env.getProperty("portal.mailjet.address.from");
-      
-      if(PortalUtils.isEmptyOrNullString(apiKey) || PortalUtils.isEmptyOrNullString(secretKey) || PortalUtils.isEmptyOrNullString(fromAddress)) {
-          log.error(EELFLoggerDelegate.errorLogger, "MailJet Credentials Not Configured ={}");
-      }
+		MailjetClient client;
+		MailjetRequest request = null;
+		MailjetResponse response = null;
 
-      client = new MailjetClient(apiKey, secretKey);
-      String text = geFreeMarkerTemplateContent(mailData.getTemplate(), mailData.getModel());
-      
-      JSONArray toJsonArray = new JSONArray();
-      
-      for (String address : mailData.getTo()) {
-    	  try {toJsonArray.put(
-			new JSONObject().put("Email", address));
-		} catch (JSONException e) {
-			log.error(EELFLoggerDelegate.errorLogger, "Exception Occurred while Sending Mail to user ={}", e);
+		String apiKey = env.getProperty("portal.mailjet.api.key");
+		String secretKey = env.getProperty("portal.mailjet.secret.key");
+		String fromAddress = env.getProperty("portal.mailjet.address.from");
+
+		if (PortalUtils.isEmptyOrNullString(apiKey))
+			log.error(EELFLoggerDelegate.errorLogger, "sendMail: failed to find required key " + apiKey);
+		if (PortalUtils.isEmptyOrNullString(secretKey))
+			log.error(EELFLoggerDelegate.errorLogger, "sendMail: failed to find required key " + secretKey);
+		if (PortalUtils.isEmptyOrNullString(fromAddress))
+			log.error(EELFLoggerDelegate.errorLogger, "sendMail: failed to find required key " + fromAddress);
+
+		client = new MailjetClient(apiKey, secretKey);
+		String text = getFreeMarkerTemplateContent(mailData.getTemplate(), mailData.getModel());
+
+		JSONArray toJsonArray = new JSONArray();
+
+		for (String address : mailData.getTo()) {
+			try {
+				toJsonArray.put(new JSONObject().put("Email", address));
+			} catch (JSONException e) {
+				log.error(EELFLoggerDelegate.errorLogger, "sendMail: failed to add address " + address);
+			}
 		}
-      }
-      request = new MailjetRequest(Email.resource)
-	                    .property(Email.FROMEMAIL, fromAddress)
-	                    //.property(Email.FROMNAME, "User Name")
-	                    .property(Email.SUBJECT, mailData.getSubject())
-	                    .property(Email.HTMLPART, text)
-	                    .property(Email.RECIPIENTS, toJsonArray);
-      log.debug(request.toString());
-      try {
-		response = client.post(request);
-	} catch (MailjetException | MailjetSocketTimeoutException e) {
-		log.error(EELFLoggerDelegate.errorLogger, "Exception Occurred while Sending Mail to user ={}", e);
-		return;
+		request = new MailjetRequest(Email.resource).property(Email.FROMEMAIL, fromAddress)
+				// .property(Email.FROMNAME, "User Name")
+				.property(Email.SUBJECT, mailData.getSubject()).property(Email.HTMLPART, text)
+				.property(Email.RECIPIENTS, toJsonArray);
+		log.debug(EELFLoggerDelegate.debugLogger, "sendMail: request: " + request.toString());
+		try {
+			response = client.post(request);
+			log.debug(EELFLoggerDelegate.debugLogger, "sendMail: client response: " + response.getData().toString());
+		} catch (MailjetException | MailjetSocketTimeoutException e) {
+			log.error(EELFLoggerDelegate.errorLogger,
+					"sendMail: failed to post email with subject " + mailData.getSubject(), e);
+			return;
+		}
 	}
-      log.debug(response.getData().toString());
-    }
-	
 
-    private String geFreeMarkerTemplateContent(String template, final Map<String, Object> model){
-        StringBuffer content = new StringBuffer();
-        try{
-           content.append(FreeMarkerTemplateUtils.processTemplateIntoString( 
-                   freemarkerConfiguration.getTemplate(template),model));
-           return content.toString();
-        }catch(Exception e){
-            
-            log.error(EELFLoggerDelegate.errorLogger, "Exception Occurred while Sending Mail to user ={}", e);
-        }
-        return "";
-    }
+	private String getFreeMarkerTemplateContent(String template, final Map<String, Object> model) {
+		StringBuffer content = new StringBuffer();
+		try {
+			content.append(FreeMarkerTemplateUtils
+					.processTemplateIntoString(freemarkerConfiguration.getTemplate(template), model));
+			return content.toString();
+		} catch (Exception e) {
+			log.error(EELFLoggerDelegate.errorLogger, "getFreeMarkerTemplateContent: failed to get content", e);
+		}
+		return "";
+	}
 }
