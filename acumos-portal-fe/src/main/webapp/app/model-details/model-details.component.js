c4af95eda75f1d3f73f99e1843ff3d45a43dc696
swood8 <SW0C74397@TechMahindra.com>
2019-05-29 11:39:17 -0500

    Add security check to license upload
    When uploading license to a model, a security check using security
    verification is now run and displays an appropriate modal if there is an
    issue.
    
    Change-Id: Ie41c7580681efb73109db641fca6851e8698b6ac
    Signed-off-by: swood8 <SW0C74397@TechMahindra.com>

--- acumos-portal-fe/src/main/webapp/app/model-details/model-details.component.js
+++ acumos-portal-fe/src/main/webapp/app/model-details/model-details.component.js
@@ -1643,28 +1643,42 @@ angular
 								var promise = modelUploadService.uploadFileToUrl($scope.file, uploadUrl);
 								
 								$scope.uploadingFile = true;
-								promise
-								.then(function(response) {
-											$scope.modelUploadError = false;
-											$rootScope.progressBar = 100;
-											$mdDialog.hide();
-											$scope.msg = "Successfully uploaded the license";
-											$scope.icon = '';
-											$scope.styleclass = 'c-success';
-											$scope.showAlertMessage = true;
-											$timeout(
-											function() {
-												$scope.showAlertMessage = false;
-											}, 5000);
-											$scope.isLicenseFound = true;
-											$scope.modelLicense = response.response_body;
-											
-										},
+								promise.then(
+									function(response) {
+										$scope.modelUploadError = false;
+										$rootScope.progressBar = 100;
+										$mdDialog.hide();
+										$scope.msg = "Successfully uploaded the license";
+										$scope.icon = '';
+										$scope.styleclass = 'c-success';
+										$scope.showAlertMessage = true;
+										$timeout(
+										function() {
+											$scope.showAlertMessage = false;
+										}, 5000);
+										$scope.isLicenseFound = true;
+										$scope.modelLicense = response.response_body;
+										
+									},
 									function(error) {
-										$scope.modelUploadError = true;
-										$scope.modelUploadErrorMsg = error.response_detail;
-										$scope.file = '';
-										$rootScope.progressBar = 0;
+										if (error.error_code == "sv_error") {
+											$mdDialog.show({
+												templateUrl : '../app/error-page/sv-modal.template.html',
+												clickOutsideToClose : true,
+												locals: { reasons: error.response_detail },
+												controller : function DialogController($scope, reasons) {
+													$scope.reasons = reasons;
+													$scope.closePoup = function(){
+														$mdDialog.hide();
+													}
+												}
+											});
+										} else {
+											$scope.modelUploadError = true;
+											$scope.modelUploadErrorMsg = error.response_detail;
+											$scope.file = '';
+											$rootScope.progressBar = 0;
+										}
 									});
 							}
 							 
