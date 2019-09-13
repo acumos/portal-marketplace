2b25d75894302ebd99960430ea699bd26d866edb
Neeraj Bhatnagar (nb7616) <NB00508933@techmahindra.com>
2019-09-13 20:26:15 +0530

    fix for Automatic Rating Add
    Change-Id: I77e6578e095fa61738b4b75a41a4c9ab33eba5fb
    Issue-ID: ACUMOS-3349
    Description:As a user, I want Acumos to automatically add a star rating while I publish my model from private to the Marketplace.
    Signed-off-by: Neeraj Bhatnagar (nb7616) <NB00508933@techmahindra.com>

--- acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
+++ acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
@@ -96,6 +96,7 @@ angular
 						$scope.showSolutionDocs = false;
 						$scope.supportingDocs = [];
 						$scope.tags1 = [];
+                   $scope.checkFlag = true;
 						componentHandler.upgradeAllRegistered();
 						$scope.solutionCompanyDescStatus = false;
 						$scope.icon = false;
@@ -502,6 +503,10 @@ angular
 									.success(
 											function(data) {
 												$scope.solutionCompanyDesc = data.response_body.description;
+												$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
+												
+												$scope.ratingCheck($scope.newDescriptionLength);
+												
 												$scope.solutionCompanyDesc1 = $sce.trustAsHtml(data.response_body.description);
 												if($scope.solutionCompanyDesc){
 													$scope.solutionCompanyDescStatus = true;
@@ -511,6 +516,7 @@ angular
 											function(data) {
 												$scope.solutionCompanyDesc = "";
 												$scope.solutionCompanyDesc1 = "";
+												$scope.newDescriptionLength = $scope.solutionCompanyDesc;
 												$scope.solutionCompanyDescStatus = false;
 											});
 						}
@@ -789,6 +795,7 @@ angular
 												$location.hash('manage-models');
 												$anchorScroll();
 												$scope.loadData();
+												$scope.getSolCompanyDesc();
 												$scope.msg = $scope.detail;
 												$scope.icon = '';
 												$scope.styleclass = 'c-success';
@@ -820,7 +827,7 @@ angular
 							if($scope.solutionCompanyDesc){
 
 								$scope.solutionCompanyDescString = $scope.solutionCompanyDesc ? String($scope.solutionCompanyDesc).replace(/<[^>]+>/gm, '') : '';
-								
+								$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
 								if($scope.solutionCompanyDesc.indexOf('src="') > -1){
 								    var newValue = $scope.solutionCompanyDesc.split('src="')[1].split('"')[0];
 								    $scope.solutionCompanyDescLength = true;
@@ -861,6 +868,11 @@ angular
 										.success(
 												function(data) {
 													$scope.solutionCompanyDesc = data.response_body.description;
+													$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
+													
+													$scope.ratingCheck($scope.newDescriptionLength);
+													
+													$scope.loadData();
 													$scope.solutionCompanyDescStatus = true;
 													$location.hash('manage-models');
 													$anchorScroll();
@@ -880,6 +892,8 @@ angular
 													$scope.solutionCompanyDesc = '';
 													$scope.solutionCompanyDescStatus = false;
 													$scope.solutionCompanyDescLength = false;
+													$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');;
+													
 												});
 							} else {
 								$location.hash('manage-models');
@@ -890,10 +904,87 @@ angular
 								$scope.showAlertMessage = true;
 								$timeout(function() {$scope.showAlertMessage = false;}, 3000);
 								$scope.solutionCompanyDesc = '';
+								
+								$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
 								$scope.showDCKEditor = !$scope.showDCKEditor;
 							}
 						}
 
+						
+						
+						
+						
+						
+						
+						// functionality for adding ratings begins
+			          			
+			          		
+			          var ratingDescription;
+						$scope.ratingCheck = function(ratingDescription){
+							$scope.rating =0;
+							$scope.rateDescriptionCheck = ratingDescription;
+														
+							$scope.ratingReview = "";							
+							if($scope.rateDescriptionCheck.length<100){
+								
+								$scope.rating = 0.1;
+							}													
+							if($scope.rateDescriptionCheck.length>100){ 
+								$scope.checkFlag = true;
+								$scope.rating = 1;
+							}
+							 if($scope.rateDescriptionCheck.length>200 ){
+								 $scope.checkFlag = true;
+								$scope.rating = 2;
+							}
+							if($scope.rateDescriptionCheck.length>300){	
+								$scope.checkFlag = true;
+								$scope.rating = 3;
+							}
+							 if($scope.rateDescriptionCheck.length>400){
+								 $scope.checkFlag = true;							 
+								 $scope.rating = 4;
+							}
+								if($scope.rateDescriptionCheck.length>500){
+									
+									$scope.rating = 5;
+								}
+							if($scope.rating){
+								var dataObjRating = {
+										"request_body" : {
+											"solutionId" : $scope.solutionId,
+											"rating" : $scope.rating,
+											"userId" : $scope.solution.ownerId,
+											"textReview": $scope.ratingReview
+										},
+										  "request_from" : "string",
+										  "request_id" : "string"
+									}
+								
+								
+								apiService.createRatingSolution(
+										dataObjRating).then(
+										function(response) {
+
+										}, function(error) {
+											//alert(error.response_detail);
+										});
+							}
+							
+							
+						}
+						
+						
+						$scope.checkMessageButton = function(){
+							$scope.checkFlag = false;
+						}
+						
+						
+						
+				
+						
+						// functionality for adding ratings ends
+												
 						$scope.copiedCompanyDesc = false;
 						$scope.copyPublicToCompany = function() {
 							var solution = {
@@ -909,6 +1000,9 @@ angular
 							$http(req).success(function(data) {
 								$scope.solutionCompanyDesc1 = data.response_body.description;
 								$scope.solutionCompanyDesc = data.response_body.description;
+								$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
+								$scope.ratingcheck($scope.newDescriptionLength);
+								//$scope.loadData();
 								$scope.copiedCompanyDesc = true;
 							});
 
@@ -962,6 +1056,10 @@ angular
 											.success(
 													function(data) {
 														$scope.solutionCompanyDesc = data.response_body.description;
+														$scope.newDescriptionLength = $scope.solutionCompanyDesc.replace(/<[^>]+>/gm, '');
+														
+														$scope.ratingcheck($scope.newDescriptionLength);
+														//$scope.loadData();
 														$scope.solutionCompanyDesc1 = data.response_body.description;
 														$scope.solutionCompanyDescStatus = true;
 														$location.hash('manage-models');
@@ -981,7 +1079,7 @@ angular
 														$scope.solutionCompanyDesc = '';
 														$scope.solutionCompanyDescStatus = false;
 														$scope.solutionCompanyDescLength = false;
-														
+														$scope.newDescriptionLength = '';
 														$location.hash('manage-models');
 														$anchorScroll();
 														$scope.getSolCompanyDesc();
