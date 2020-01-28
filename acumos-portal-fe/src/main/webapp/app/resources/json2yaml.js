c2429491bca6157d7c0c82df4cc7b0c78c97e369
mcapraful <PD00618320@TechMahindra.com>
2020-01-28 13:27:25 +0530

    license profile in a human readable format
    Change-Id: I79f49c02b6c845516ceb47e6206923859c199947
    Issue-Id: ACUMOS-3749
    Description: As a model user, I would like to view the license profile in a human readable format
    Signed-off-by: mcapraful <PD00618320@TechMahindra.com>

---
+++ acumos-portal-fe/src/main/webapp/app/resources/json2yaml.js
@@ -0,0 +1,125 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+(function (self) {
+  /*
+   * TODO, lots of concatenation (slow in js)
+   */
+  var spacing = "  ";
+
+  function getType(obj) {
+    var type = typeof obj;
+    if (obj instanceof Array) {
+      return 'array';
+    } else if (type == 'string') {
+      return 'string';
+    } else if (type == 'boolean') {
+      return 'boolean';
+    } else if (type == 'number') {
+      return 'number';
+    } else if (type == 'undefined' || obj === null) {
+      return 'null';
+    } else {
+      return 'hash';
+    }
+  }
+
+  function convert(obj, ret) {
+    var type = getType(obj);
+
+    switch(type) {
+      case 'array':
+        convertArray(obj, ret);
+        break;
+      case 'hash':
+        convertHash(obj, ret);
+        break;
+      case 'string':
+        convertString(obj, ret);
+        break;
+      case 'null':
+        ret.push('null');
+        break;
+      case 'number':
+        ret.push(obj.toString());
+        break;
+      case 'boolean':
+        ret.push(obj ? 'true' : 'false');
+        break;
+    }
+  }
+
+  function convertArray(obj, ret) {
+    if (obj.length === 0) {
+      ret.push('[]');
+    }
+    for (var i=0; i<obj.length; i++) {
+
+      var ele     = obj[i];
+      var recurse = [];
+      convert(ele, recurse);
+
+      for (var j=0; j<recurse.length; j++) {
+        ret.push((j == 0 ? "- " : spacing) + recurse[j]);
+      }
+    }
+  }
+
+  function convertHash(obj, ret) {
+    for (var k in obj) {
+      var recurse = [];
+      if (obj.hasOwnProperty(k)) {
+        var ele = obj[k];
+        convert(ele, recurse);
+        var type = getType(ele);
+        if (type == 'string' || type == 'null' || type == 'number' || type == 'boolean') {
+          ret.push("<span><strong>"+ normalizeString(k) + ': </strong></span>' +  recurse[0]);
+        } else {
+          ret.push("<span class='seperate-parent'><strong>"+normalizeString(k) + '</strong></span>');
+          for (var i=0; i<recurse.length; i++) {
+            ret.push(spacing + recurse[i]);
+          }
+        }
+      }
+    }
+  }
+
+  function normalizeString(str) {
+    if (str.match(/^[\w\s]+$/)) {
+      return str;
+    } else {
+      return (str).replace(/%u/g,'\\u').replace(/%U/g,'\\U').replace(/%/g,'\\x');
+    }
+  }
+
+  function convertString(obj, ret) {
+    ret.push(normalizeString(obj));
+  }
+
+  self.json2yaml = function(obj) {
+    if (typeof obj == 'string') {
+      obj = JSON.parse(obj);
+    }
+
+    var ret = [];
+    convert(obj, ret);
+    return ret.join("\n");
+  };
+})(this);
