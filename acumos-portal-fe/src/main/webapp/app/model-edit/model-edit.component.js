98c98958409ffcf4e510150268ed3f220467d9df
mcapraful <PD00618320@TechMahindra.com>
2020-02-06 16:20:39 +0530

    Preview model using older template license profile
    Change-Id: Ifbcb5d03404f328aee95f159586265ab7b45350a
    Issue-Id: ACUMOS-3968
    Description: Preview model view using older template of license profile
    Signed-off-by: mcapraful <PD00618320@TechMahindra.com>

--- acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
+++ acumos-portal-fe/src/main/webapp/app/model-edit/model-edit.component.js
@@ -2830,8 +2830,11 @@ angular
 								url : url
 						}).then(function successCallback(response) {
 							console.log(response);
-							if (response.data) {
+							if (response.data) {								
 								$scope.modelLicense = response.data;
+                                                          	$scope.humanReadableModelLicense = angular.copy($scope.modelLicense);
+								delete $scope.humanReadableModelLicense["$schema"]; //to remove $schema key
+								$scope.yaml = json2yaml($scope.humanReadableModelLicense);
 								$scope.isLicensePresent = true;
 								$scope.licenseName = "license-"+$scope.versionId+".json";
 							} else {
